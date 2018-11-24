675c511714b9dda751313eab88af5b8ce5c1f337
lgaric <lgaric@foi.hr>
2018-11-24 18:29:10 +0100

    Progress Bar i provjera email-a
    Dodan progress bar zbog malo dužeg vremena koje je potrebno da firebase odradi zahtjev. Poboljšan feedback korisniku. Dodana provjera ispravnosti emaila (ukoliko je mail neispravan ne šalje se zahtjev!)

--- app/src/main/java/hr/foi/air/mygrocerypal/myapplication/Controller/PasswordRecoveryActivity.java
+++ app/src/main/java/hr/foi/air/mygrocerypal/myapplication/Controller/PasswordRecoveryActivity.java
@@ -3,9 +3,11 @@ package hr.foi.air.mygrocerypal.myapplication.Controller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,6 +19,7 @@ import hr.foi.air.mygrocerypal.myapplication.R;
 import hr.foi.air.mygrocerypal.myapplication.View.LoginActivity;
 
 public class PasswordRecoveryActivity extends BaseActivity/* implements View.OnClickListener*/ {
+    private ProgressBar progressBar;
     private EditText userEmail;
     private Button buttonRecoveryPassword, showLogin;
     FirebaseAuth firebaseAuth;
@@ -28,6 +31,7 @@ public class PasswordRecoveryActivity extends BaseActivity/* implements View.OnC
 
         buttonRecoveryPassword = findViewById(R.id.buttonRecoveryPassword);
         showLogin = findViewById(R.id.buttonShowLogin);
+        progressBar = findViewById(R.id.progressBar);
 
         userEmail =(EditText) findViewById(R.id.emailRecoveryPassword);
         firebaseAuth = FirebaseAuth.getInstance();
@@ -35,19 +39,25 @@ public class PasswordRecoveryActivity extends BaseActivity/* implements View.OnC
         buttonRecoveryPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                firebaseAuth.sendPasswordResetEmail(userEmail.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if(task.isSuccessful()){
-                                    Toast.makeText(PasswordRecoveryActivity.this, "Link za promjenu lozinke je poslan. Provjerite email!", Toast.LENGTH_LONG).show();
-                                    ShowActivity(LoginActivity.class);
+                if(validateEmail(userEmail)){
+                    progressBar.setVisibility(View.VISIBLE);
+                    firebaseAuth.sendPasswordResetEmail(userEmail.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    progressBar.setVisibility(View.GONE);
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(PasswordRecoveryActivity.this, "Link za promjenu lozinke je poslan. Provjerite email!", Toast.LENGTH_LONG).show();
+                                        ShowActivity(LoginActivity.class);
+                                    }
+                                    else{
+                                        Toast.makeText(PasswordRecoveryActivity.this, "Došlo je do greške, provjerite ispravnost email-a!", Toast.LENGTH_LONG).show();
+                                    }
                                 }
-                                else{
-                                    Toast.makeText(PasswordRecoveryActivity.this, "Došlo je do greške, provjerite ispravnost email-a!", Toast.LENGTH_LONG).show();
-                                }
-                            }
-                        });
+                            });
+                }else{
+                    Toast.makeText(PasswordRecoveryActivity.this, "Unesite ispravnu email adresu!", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -60,7 +70,17 @@ public class PasswordRecoveryActivity extends BaseActivity/* implements View.OnC
 
 
     }
-    
+
+    private boolean validateEmail(EditText emailTxt){
+        String email = emailTxt.getText().toString().trim();
+
+        if(email == null){
+            return false;
+        } else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            return false;
+        }else return true;
+    }
+
 
 /*
     @Override
