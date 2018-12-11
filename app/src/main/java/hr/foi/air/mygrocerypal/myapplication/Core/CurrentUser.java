369971707c0b36cebad0e47be684019dfe21f39c
vgrbavac <vgrbavac@foi.hr>
2018-12-11 22:27:34 +0100

    Pokušaj quick fix-a

--- app/src/main/java/hr/foi/air/mygrocerypal/myapplication/Core/CurrentUser.java
+++ app/src/main/java/hr/foi/air/mygrocerypal/myapplication/Core/CurrentUser.java
@@ -2,6 +2,7 @@ package hr.foi.air.mygrocerypal.myapplication.Core;
 
 import hr.foi.air.mygrocerypal.myapplication.Model.UserModel;
 
+
 public class CurrentUser {
     public static UserModel currentUser;
 }
