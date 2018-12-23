13c017cf9c37e791596282ed0ea25cb7263a765a
Bozo Culo <bculo@foi.hr>
2018-12-23 11:58:09 +0100

    Napravljen modul za filtriranje proizvoda po kategoriji i po imenu
    Korištene klase -> FilterableObject, FilterObject
    Interfejsi -> ObjectsFilterListener
    Enumi -> FilterType

---
+++ filter/src/main/java/com/example/filter/FilterableObject.java
@@ -0,0 +1,9 @@
+package com.example.filter;
+
+/**
+ * Moguce je filtrirati klase samo koje nasljeduju FilterableObject
+ */
+public abstract class FilterableObject {
+    protected String name;
+    protected String category_name;
+}
