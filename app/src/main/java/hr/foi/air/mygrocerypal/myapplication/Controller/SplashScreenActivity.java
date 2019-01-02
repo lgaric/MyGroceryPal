448818145817c1e5a07a40bf2a66d3486067c9cd
Vedran Grbavac <vgrbavac@foi.hr>
2019-01-02 14:28:17 +0100

    Napravljen SplashScreen prilikom otvaranja aplikacije
    Napravljen SplashScreen koji pokazuje logo aplikacije prilikom otvaranje aplikacije. SplashScreen se pokazuje dok se aplikacija ne učita, ukoliko je aplikacije već u cache memoriji SplashScreen traje jako kratko.

---
+++ app/src/main/java/hr/foi/air/mygrocerypal/myapplication/Controller/SplashScreenActivity.java
@@ -0,0 +1,21 @@
+package hr.foi.air.mygrocerypal.myapplication.Controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import hr.foi.air.mygrocerypal.myapplication.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Intent intent = new Intent(getApplicationContext(),
+                MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
